c0fa82d85708ed347627d5230a628e0c11e1605c
cerdemrc <cerdemrc@gmail.com>
2022-01-31 16:18:00 +0300

    install chakra ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
 import '../public/styles/globals.css';
+import { ChakraProvider } from '@chakra-ui/react';
 
 export default function App({ Component, pageProps }) {
 	return (
-		<>
+		<ChakraProvider>
 			<Component {...pageProps} />
-		</>
+		</ChakraProvider>
 	);
 }
