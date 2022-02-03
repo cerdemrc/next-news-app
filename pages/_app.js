27c20c51d3a87b84eae062c589d76bada18b34a7
cerdemrc <cerdemrc@gmail.com>
2022-02-03 11:43:08 +0300

    remove: chakra

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,13 @@
 import '../public/styles/globals.scss';
-import { ChakraProvider } from '@chakra-ui/react';
 import Header from '../components/Header/Header';
 import { Provider } from 'react-redux';
 import { store } from '../redux/store';
 
 export default function App({ Component, pageProps }) {
 	return (
-		<ChakraProvider>
-			<Provider store={store}>
-				<Header />
-				<Component {...pageProps} />
-			</Provider>
-		</ChakraProvider>
+		<Provider store={store}>
+			<Header />
+			<Component {...pageProps} />
+		</Provider>
 	);
 }
