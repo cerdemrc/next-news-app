b02923ab599e3ff1ef5f86c2638b05fd92ed36f7
cerdemrc <cerdemrc@gmail.com>
2022-02-03 14:42:49 +0300

    feat: add new pages

---
+++ pages/startup.js
@@ -0,0 +1,9 @@
+import News from '../components/News';
+import { useRouter } from 'next/router';
+
+export default function startup() {
+	const router = useRouter();
+	const category = router.route.split('/')[1];
+
+	return <News category={category} />;
+}
