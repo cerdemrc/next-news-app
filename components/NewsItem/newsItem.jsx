d921dcb6c56e273e14818e20abf54c8493be9e1c
Ceren Demirci <cerdemrc@gmail.com>
2023-04-13 22:46:05 +0300

    fix: news item box look

--- components/NewsItem/newsItem.jsx
+++ components/NewsItem/newsItem.jsx
@@ -2,17 +2,12 @@ import styles from './newsItem.module.scss';
 
 export default function News({ item }) {
 	return (
-		<div className='relative pb-14'>
-			<img src={item.imageUrl} />
+		<a className='relative rounded-3xl' href={item.url}>
+			<img className={styles.newsImage} src={item.imageUrl} />
 			<div className={styles.newsCard}>
 				<h5 className={styles.date}>{item.date}</h5>
 				<h1 className={styles.title}>{item.title}</h1>
-				<p className={styles.content}>{item.content}</p>
-				<h5 className={styles.author}>Author: {item.author}</h5>
-				<a className={styles.url} href={item.url}>
-					Read Me
-				</a>
 			</div>
-		</div>
+		</a>
 	);
 }
